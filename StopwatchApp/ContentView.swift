a140d320e7cdf4c4ec488cb1229259423e7357b7
Russell Gordon <rgordon@lcs.on.ca>
2022-11-06 14:41:53 -0500

    Changed to more familiar syntax for creating a background.

--- StopwatchApp/ContentView.swift
+++ StopwatchApp/ContentView.swift
@@ -11,8 +11,8 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             // First layer
-            Color.pink.ignoresSafeArea()
-            
+            Color(UIColor.red)
+                .ignoresSafeArea()            
             
             // Second layer
             VStack {
