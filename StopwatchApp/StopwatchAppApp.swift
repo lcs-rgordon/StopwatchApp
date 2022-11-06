6583534d22d874e36c2845dd86601e0eaad1118b
Russell Gordon <rgordon@lcs.on.ca>
2022-11-06 14:34:59 -0500

    Initial Commit

---
+++ StopwatchApp/StopwatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchAppApp.swift
+//  StopwatchApp
+//
+//  Created by Russell Gordon on 2022-11-06.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
